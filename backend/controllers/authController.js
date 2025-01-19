cbea35c7491b68a751eabf8ec200b65b63650c13
yihfei <lim.yih.fei@gmail.com>
2025-01-19 21:08:28 +0800

    Create POST request for sign up

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,35 @@
-
+const User = require('../models/user');
 
 const test = (req, res) => {
     res.json("test is working");
     console.log("hihi")
 }
 
+const registerUser = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        if (!name) {
+            return res.status(400).json({msg: "Name is required"});
+        }
+        if (!password || password.length < 6) {
+            return res.status(400).json({msg: "Password is required and should be at least 6 characters long"});
+        }
+        const exist = await User.findOne({email});
+        if (exist) {
+            return res.status(400).json({msg: "Email already exists"});
+        }
+
+        const user = await User.create({
+            name, email, password
+        });
+
+        return res.json(user);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
-    test
+    test,
+    registerUser
 }
